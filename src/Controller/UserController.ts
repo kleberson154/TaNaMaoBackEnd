043fb1b364af26b7e32df24bfa8e5b844df33045
kleberson154 <kleberson55@hotmail.com>
2025-10-15 16:04:19 -0300

    feat: Criando novas funcoes

--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -2,11 +2,19 @@ import { Response } from 'express'
 import { AuthRequest } from '../types/express'
 import User, { IUser } from '../models/User'
 import jwt from 'jsonwebtoken'
+import Produto from '../models/Produto'
+import AvaliacaoProduto from '../models/AvaliacaoProduto'
+import UserCarrinho from '../models/UserCarrinho'
 
 class UserController {
   async register(req: AuthRequest, res: Response): Promise<Response | void> {
     try {
-      const { email, senha, nome, sobrenome, telefone } = req.body
+      const { nome, cpf, email, senha, confirmacaoSenha } = req.body
+
+      if (senha !== confirmacaoSenha) {
+        res.status(400).json({ error: 'As senhas não coincidem' })
+        return
+      }
 
       const existingUser = await User.findOne({ email })
       if (existingUser) {
@@ -14,12 +22,17 @@ class UserController {
         return
       }
 
+      const existingCPF = await User.findOne({ cpf })
+      if (existingCPF) {
+        res.status(409).json({ error: 'CPF já cadastrado' })
+        return
+      }
+
       const newUser: IUser = new User({
+        nome,
+        cpf,
         email,
-        password: senha,
-        firstName: nome,
-        lastName: sobrenome,
-        phone: telefone
+        senha
       })
 
       await newUser.save()
@@ -49,7 +62,7 @@ class UserController {
       }
 
       const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
-        expiresIn: '1h'
+        expiresIn: '24h'
       })
 
       const { password: _, ...safeUser } = user.toObject()
@@ -66,9 +79,92 @@ class UserController {
         res.status(404).json({ error: 'Usuário não encontrado' })
         return
       }
-      res.status(200).json({ user })
+      let carrinho = await UserCarrinho.findOne({ idUsuario: user._id })
+      if (!carrinho) {
+        carrinho = new UserCarrinho({ idUsuario: user._id, produtos: [] })
+      }
+      res.status(200).json({ user, carrinho })
     } catch (error) {
       res.status(500).json({ error: 'Erro ao buscar dados do usuário' })
+    }
+  }
+
+  async getSearch(req: AuthRequest, res: Response): Promise<Response | void> {
+    try {
+      const { query } = req.params
+      const produtos = await Produto.find({
+        nome: { $regex: query, $options: 'i' }
+      })
+      res.status(200).json({ produtos })
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao buscar produtos' })
+    }
+  }
+
+  async getProduto(req: AuthRequest, res: Response): Promise<Response | void> {
+    try {
+      const produto = await Produto.findById(req.params.id)
+      if (!produto) {
+        res.status(404).json({ error: 'Produto não encontrado' })
+        return
+      }
+      const avaliacaoProduto = await AvaliacaoProduto.find({
+        idProduto: produto._id
+      })
+      res.status(200).json({ produto, avaliacaoProduto })
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao buscar dados do produto' })
+    }
+  }
+
+  async createProduto(
+    req: AuthRequest,
+    res: Response
+  ): Promise<Response | void> {
+    try {
+      const {
+        idVendedor,
+        nome,
+        categoria,
+        precoCompra,
+        precoAluguel,
+        quantidade,
+        descricao,
+        imagem
+      } = req.body
+      const novoProduto = new Produto({
+        idVendedor,
+        nome,
+        categoria,
+        precoCompra,
+        precoAluguel,
+        quantidade,
+        descricao,
+        imagem
+      })
+      await novoProduto.save()
+      res.status(201).json({ produto: novoProduto })
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao criar produto' })
+    }
+  }
+
+  async createAvaliacao(
+    req: AuthRequest,
+    res: Response
+  ): Promise<Response | void> {
+    try {
+      const { idProduto, idUsuario, nota, comentario } = req.body
+      const novaAvaliacao = new AvaliacaoProduto({
+        idProduto,
+        idUsuario,
+        nota,
+        comentario
+      })
+      await novaAvaliacao.save()
+      res.status(201).json({ avaliacao: novaAvaliacao })
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao criar avaliação' })
     }
   }
 }
