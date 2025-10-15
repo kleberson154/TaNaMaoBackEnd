700b37c72fa409764ccff3f5ebb4660d2c807d1b
kleberson154 <kleberson55@hotmail.com>
2025-10-15 16:03:49 -0300

    feat: Criando mais rotas para as novas funcoes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,21 +5,39 @@ import { authMiddleware } from '../middlewares/authMiddleware'
 
 const router: Router = Router()
 
-router.post('/register', async (req, res) => {
+router.get('/', async (req, res) => {})
+
+router.post('/cadastro', async (req, res) => {
   await UserController.register(req, res)
 })
 
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
   await UserController.login(req, res)
 })
 
-router.get('/home', authMiddleware, async (req, res) => {
-  try {
-    const user = await UserController.getUserData(req, res)
-    res.json(user)
-  } catch (err: any) {
-    res.status(500).json({ error: err.message })
+router.get('/perfil', authMiddleware, async (req, res) => {
+  const userId = req.user?.id
+  if (!userId) {
+    return res.status(401).json({ error: 'Usuário não autenticado' })
   }
+
+  try {
+    const user: IUser | null = await User.findById(userId).select('-senha')
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado' })
+    }
+    res.status(200).json({ user })
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar dados do usuário' })
+  }
+})
+
+router.get('/produtos/:id', async (req, res) => {
+  await UserController.getProduto(req, res)
+})
+
+router.get('/produtos/busca/:query', async (req, res) => {
+  await UserController.getSearch(req, res)
 })
 
 export default router
