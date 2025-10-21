e7515bda462d1dc0bc31821bc41cdc7a89250c4c
kleberson154 <kleberson55@hotmail.com>
2025-10-21 15:03:28 -0300

    refactor: apagando classes que foram inseridas no User e Produtos

--- src/models/Pedidos.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Document, Schema } from 'mongoose'
-
-export interface IPedido extends Document {
-  idComprador: mongoose.Types.ObjectId
-  idVendedor: mongoose.Types.ObjectId
-  idProduto: mongoose.Types.ObjectId
-  quantidade: number
-  status: 'preparando' | 'enviado' | 'concluido'
-}
