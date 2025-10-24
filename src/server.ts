1729d2fb0d8d0cb81f4d4a6ff27947b75c0446cc
kleberson154 <kleberson55@hotmail.com>
2025-10-24 15:52:15 -0300

    feat: adicionando credenciais no CORS

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,8 @@ app.use(
   cors({
     origin: ['https://localhost:3000', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   })
 )
 app.use(express.json())
